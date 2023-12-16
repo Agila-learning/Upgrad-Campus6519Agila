b22d74fd97513d2777035b3909c6d6fa67fde330
Agila-learning <125821156+Agila-learning@users.noreply.github.com>
2023-12-16 22:51:25 +0530

    Add files via upload
    String array using for loop

---
+++ ArrayCode.java
@@ -0,0 +1,13 @@
+public class ArrayCode {
+    public static void main(String[] args) {
+        String [] Learn=new String[4];
+        Learn[0]="Lectiophile";
+        Learn[1]="Selenophile";
+        Learn[2]="Aviphile";
+        for(int i=0;i<=2;i++){
+            if(Learn [i]!=null){
+                System.out.println(Learn[i]);
+            }
+        }
+    }
+}

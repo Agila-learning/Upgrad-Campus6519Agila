83e573f1e8b5abe22f1899fb92e27738b1f693d4
Agila-learning <125821156+Agila-learning@users.noreply.github.com>
2023-12-16 22:38:02 +0530

    Add files via upload
    Program on array concept

---
+++ ArrayProg.java
@@ -0,0 +1,10 @@
+public class ArrayProg {
+    public static void main(String[] args) {
+        //int []arr=new int[4]; //Array dec;aration type 1
+        int []arr={23,56,78,9}; //array declaration type 2
+        // arr[0]=12;
+        // arr[1]=34;
+        // arr[2]=11;
+        System.out.println(arr[1]);
+    }
+}
